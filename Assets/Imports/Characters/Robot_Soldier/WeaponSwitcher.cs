02146feae8b3b0fa17850e2a74d83b66782819cc
Robin-462 <ruochenzheng4@gmail.com>
2024-08-21 06:32:32 +0800

    Assets

--- Assets/Imports/Characters/Robot_Soldier/WeaponSwitcher.cs
+++ Assets/Imports/Characters/Robot_Soldier/WeaponSwitcher.cs
@@ -7,20 +7,23 @@ public class WeaponSwitcher : MonoBehaviour
     public GameObject defaultWeapon;
 
     private GameObject currentWeapon;
+    private Transform rightHandMiddle1;
 
     void Start()
     {
         currentWeapon = defaultWeapon;
         sword.SetActive(false);
+        Debug.Log("Setting sword inactive.");
         shotgun.SetActive(false);
         defaultWeapon.SetActive(true);
+        rightHandMiddle1 = GameObject.Find("RightHandMiddle4").transform;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            SwitchWeapon(sword);
+            SwitchWeapon(defaultWeapon);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
@@ -28,14 +31,35 @@ public class WeaponSwitcher : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            SwitchWeapon(defaultWeapon);
+            SwitchWeapon(sword);
         }
+
+        FollowHand();
     }
 
     void SwitchWeapon(GameObject newWeapon)
     {
-        currentWeapon.SetActive(false);
+        if (currentWeapon!= null)
+        {
+            currentWeapon.SetActive(false);
+            Debug.Log("Disabling previous weapon. Current weapon active status: " + currentWeapon.activeSelf);
+        }
+
         newWeapon.SetActive(true);
         currentWeapon = newWeapon;
+
+        if (currentWeapon == sword)
+        {
+            Debug.Log("Switching to sword. Sword active status: " + sword.activeSelf);
+        }
+    }
+
+    void FollowHand()
+    {
+        if (currentWeapon!= null)
+        {
+            currentWeapon.transform.position = rightHandMiddle1.position;
+            currentWeapon.transform.rotation = rightHandMiddle1.rotation;
+        }
     }
 }
