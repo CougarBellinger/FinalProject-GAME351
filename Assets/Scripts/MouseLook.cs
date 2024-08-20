f0c2b5cfc2b3fa48cb7d0467004846203e0eb69d
CougarBellinger <cougarbellinger@gmail.com>
2024-08-20 01:07:15 -0700

    Finished movement and looking

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform character;
+    private float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //transform.localRotation = Quaternion.Euler(0f, mouseX, 0f);
+        character.Rotate(Vector3.up * mouseX);
+    }
+}
