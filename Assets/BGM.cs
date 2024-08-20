cb2882156e34ded7dd236b32b7eddbf8669abbf4
mvogel521 <mileeavogel123@gmail.com>
2024-08-19 23:55:22 -0700

    BGM script

---
+++ Assets/BGM.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip defaultMusic;
+    public AudioClip fightMusic;
+    
+    private AudioSource audioSource;
+    private bool hasSwitchedToFightMusic = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = defaultMusic;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( (Input.GetMouseButton(0)) || (Input.GetKeyDown(KeyCode.F)) || (Input.GetKeyDown(KeyCode.Q)) ){
+            if(!hasSwitchedToFightMusic){
+                SwitchToFightMusic();
+                hasSwitchedToFightMusic = true;
+            }
+        }
+        else{
+            SwitchToDefaultMusic();
+        }
+    }
+
+private void SwitchToDefaultMusic(){
+    audioSource.Stop();
+    audioSource.clip = defaultMusic;
+    audioSource.loop = true;
+    audioSource.Play();
+}
+
+ private void SwitchToFightMusic(){
+    audioSource.Stop();
+    audioSource.clip = fightMusic;
+    audioSource.loop = true;
+    audioSource.Play();
+}
+
+}
+
