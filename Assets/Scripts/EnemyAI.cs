52866aceda4a145257344f920e862631ad9a4a9c
Robin-462 <ruochenzheng4@gmail.com>
2024-08-23 02:08:02 +0800

    Assets

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,83 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyShooting : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
-    public GameObject muzzleFlashEffect;
-    private EnemyAI enemyAI;
-    private ParticleSystem muzzleParticleSystem;
+    public NavMeshAgent agent;
+    public GameObject player;
+    public float wanderRadius;
+    public float detectionRadius;
+    public float wanderTimer;
+    public GameObject gunJoint;
+    public GameObject gun;
+    public float rotationSpeed = 5f;
 
-    void StartFunction()
+    private Transform target;
+    private float timer;
+
+    void Start()
     {
-        if (muzzleFlashEffect!= null)
+        agent = GetComponent<NavMeshAgent>();
+        target = player.transform;
+        timer = wanderTimer;
+
+        agent.updateRotation = false;
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, target.position);
+
+        if (distanceToPlayer <= detectionRadius)
         {
-            muzzleParticleSystem = muzzleFlashEffect.GetComponent<ParticleSystem>();
-            if (muzzleParticleSystem == null)
-            {
-                Debug.LogError("ParticleSystem component not found on muzzleFlashEffect.");
-            }
-            else
-            {
-                muzzleParticleSystem.Stop();
-            }
+            agent.SetDestination(target.position);
+            RotateTowardsPlayer();
         }
         else
         {
-            Debug.LogError("muzzleFlashEffect is not assigned.");
-        }
-
-        enemyAI = GetComponent<EnemyAI>();
-        if (enemyAI == null)
-        {
-            Debug.LogError("EnemyAI component not found.");
-        }
-    }
-
-    void UpdateFunction()
-    {
-        if (enemyAI!= null)
-        {
-            if (muzzleParticleSystem!= null)
+            if (timer <= 0)
             {
-                bool isChasing = IsChasingPlayer();
-                if (isChasing)
-                {
-                    StartShooting();
-                }
-                else
-                {
-                    StopShooting();
-                }
+                Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                agent.SetDestination(newPos);
+                timer = wanderTimer;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
             }
         }
+
+        RotateGunJointTowardsPlayer();
+        AlignGunWithGunJoint();
     }
 
-    void StartShootingFunction()
+    void RotateTowardsPlayer()
     {
-        if (muzzleParticleSystem!= null)
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        directionToPlayer.y = 0;
+
+        if (directionToPlayer != Vector3.zero)
         {
-            if (!muzzleParticleSystem.isPlaying)
-            {
-                Debug.Log("Starting muzzle flash effect.");
-                muzzleParticleSystem.Play();
-            }
+            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
         }
     }
 
-    void StopShootingFunction()
+    void RotateGunJointTowardsPlayer()
     {
-        if (muzzleParticleSystem!= null)
+        Vector3 directionToPlayer = player.transform.position - gunJoint.transform.position;
+
+        if (directionToPlayer != Vector3.zero)
         {
-            if (muzzleParticleSystem.isPlaying)
-            {
-                Debug.Log("Stopping muzzle flash effect.");
-                muzzleParticleSystem.Stop();
-            }
+            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+            gunJoint.transform.rotation = Quaternion.Slerp(gunJoint.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
         }
     }
 
-    bool IsChasingPlayerFunction()
+    void AlignGunWithGunJoint()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, enemyAI.player.transform.position);
-        return distanceToPlayer <= enemyAI.detectionRadius;
+        if (gunJoint != null)
+        {
+            gun.transform.rotation = gunJoint.transform.rotation;
+        }
+    }
+
+    static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+        randDirection += origin;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randDirection, out hit, dist, NavMesh.AllAreas);
+        return hit.position;
     }
 }
