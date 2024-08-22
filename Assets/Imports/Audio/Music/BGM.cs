ea5a04926adb0f9ff29cc0b7060e1baf28f37532
staruntracer <mle4@arizona.edu>
2024-08-22 14:47:22 -0700

    audui switch test

--- Assets/Imports/Audio/Music/BGM.cs
+++ Assets/Imports/Audio/Music/BGM.cs
@@ -1,36 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class BGM : MonoBehaviour
 {
+    public GameObject player;
+    private Transform target;
     public AudioClip defaultMusic;
     public AudioClip fightMusic;
 
     private AudioSource audioSource;
     private bool hasSwitchedToFightMusic = false;
 
+    public float detectionRadius;
+    
+
     // Start is called before the first frame update
 
     // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log("Calling Update");
+    void Update(){
+        Vector3 targetPosition = player.transform.position;
+        // targetPosition.y = fixedHeight;
+        float distanceToPlayer = Vector3.Distance(transform.position, targetPosition);
         if ((Input.GetMouseButton(0)) || (Input.GetKeyDown(KeyCode.F)) || (Input.GetKeyDown(KeyCode.Q)))
         {
             if (!hasSwitchedToFightMusic)
             {
                 SwitchToFightMusic();
                 hasSwitchedToFightMusic = true;
-
-                Debug.Log("Invoked input");
             }
         }
         else
         {
-            //SwitchToDefaultMusic();
+            if ((hasSwitchedToFightMusic) && (distanceToPlayer > 10))
+            {
+                SwitchToDefaultMusic();
+                hasSwitchedToFightMusic = false;
+            }
         }
     }
+    // {
+    //     //Debug.Log("Calling Update");
+    //     if ((Input.GetMouseButton(0)) || (Input.GetKeyDown(KeyCode.F)) || (Input.GetKeyDown(KeyCode.Q)))
+    //     {
+    //         if (!hasSwitchedToFightMusic)
+    //         {
+    //             SwitchToFightMusic();
+    //             hasSwitchedToFightMusic = true;
+
+    //             Debug.Log("Invoked input");
+    //         }
+    //     }
+    //     else
+    //     {
+    //         float distanceToPlayer = Vector3.Distance(transform.position, targetPosition);
+            
+    //         if ((hasSwitchedToFightMusic) && (distanceToPlayer > detectionRadius))
+    //         {
+    //             SwitchToDefaultMusic();
+    //             hasSwitchedToFightMusic = false;
+    //         }
+    //     }
+    // }
 
     private void SwitchToDefaultMusic()
     {
