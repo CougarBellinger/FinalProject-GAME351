2f150ac8f8822d747af15526f61c598f1ffd6fc1
Robin-462 <ruochenzheng4@gmail.com>
2024-08-23 00:12:44 +0800

    Update Translate.cs

--- Assets/Scripts/Translate.cs
+++ Assets/Scripts/Translate.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 public class Translate : MonoBehaviour
 {
     public Transform transformPos;
-    // Start is called before the first frame update
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        this.transform.translation(transformPos.position.forward);
-
-        this.transform.rotation(transformPos.position.up);
-        //this.transform.position.x = transformPos.position.x;
+        transform.position = transformPos.position;
+        transform.rotation = Quaternion.LookRotation(transformPos.forward, transformPos.up);
     }
 }
